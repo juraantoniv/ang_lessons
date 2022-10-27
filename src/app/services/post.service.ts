6a51924e1bf39e0edcb5564ab3b583a7c16ae724
juraantoniv <juraantoniv@gmail.com>
2022-10-27 10:48:12 +0300

    ok

---
+++ src/app/services/post.service.ts
@@ -0,0 +1,24 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+import {urls} from '../configs';
+import {IPost} from "../interfeces";
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  getAll(): Observable<IPost[]> {
+    return this.httpClient.get<IPost[]>(urls.posts)
+  }
+
+  // getById(id: number): Observable<IUser> {
+  //   return this.httpClient.get<IUser>(`${urls.users}/${id}`)
+  // }
+}
